a361060519685bf214cee6bf5df777c99739394c
Alain Armand <aarmand.inbox@gmail.com>
2017-12-20 17:29:45 +0100

    changes back to og dir format

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
-const path = require('path');
+const HtmlPlugin = require('webpack-html-plugin')
+const join = require('path').join
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: './src/index.bs.js',
+const stats = {
+  chunks: false,
+  modules: false,
+}
+
+module.exports = [{
+  entry: './src/client',
   output: {
-    path: path.join(__dirname, "build"),
-    filename: 'index.js',
+    filename: 'index.re',
+    path: join(__dirname, 'bundle'),
+    publicPath: '/',
   },
-};
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html',
+      inject: true
+    }),
+    new ExtractTextPlugin("styles.css")
+  ],
+  module: {
+    rules: [{
+      test: /\.(re|ml)$/,
+      use: 'bs-loader'
+    }, {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: "css-loader"
+      })
+    }],
+  },
+  resolve: {
+    extensions: ['.re', '.ml', '.js']
+  },
+  devServer: {
+    stats
+  }
+}]
